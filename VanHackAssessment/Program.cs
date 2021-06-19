da236faf905a9462d47683925fe430af48e7538f
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2021-06-19 18:00:51 -0300

    Initial code

---
+++ VanHackAssessment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VanHackAssessment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Numerals(1);
+            Numerals(4);
+            Numerals(6);
+            Numerals(89);
+            Numerals(91);
+            Numerals(1889);
+        }
+
+        public static string Numerals(int num)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
