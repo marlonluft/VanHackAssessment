7989b5e7d52eeffc047a31ec60334e9e284a5b84
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2021-06-19 19:47:12 -0300

    Program call challgenge class

--- VanHackAssessment/Program.cs
+++ VanHackAssessment/Program.cs
@@ -1,22 +1,17 @@
-﻿using System;
-
-namespace VanHackAssessment
+﻿namespace VanHackAssessment
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Numerals(1);
-            Numerals(4);
-            Numerals(6);
-            Numerals(89);
-            Numerals(91);
-            Numerals(1889);
-        }
+            var challenge = new Challenge();
 
-        public static string Numerals(int num)
-        {
-            throw new NotImplementedException();
+            challenge.Numerals(1889);
+            challenge.Numerals(91);
+            challenge.Numerals(89);
+            challenge.Numerals(6);
+            challenge.Numerals(4);
+            challenge.Numerals(1);
         }
     }
 }
