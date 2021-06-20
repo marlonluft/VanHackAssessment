c2413d3a3c8166effa564c02a517fe535552e99f
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2021-06-19 22:33:55 -0300

    Show returned value in the message error

--- VanHackAssessment.Test/ChallengeTest.cs
+++ VanHackAssessment.Test/ChallengeTest.cs
@@ -8,42 +8,54 @@ namespace VanHackAssessment.Test
         public void ShouldBeI()
         {
             var challenge = new Challenge();
-            Assert.True("I" == challenge.Numerals(1), "Roman numeral for 1 is incorrect");
+            var roman = challenge.Numerals(1);
+
+            Assert.True("I" == roman, $"Roman numeral {roman} for 1 is incorrect");
         }
 
         [Fact]
         public void ShouldBeIV()
         {
             var challenge = new Challenge();
-            Assert.True("IV" == challenge.Numerals(4), "Roman numeral for 4 is incorrect");
+            var roman = challenge.Numerals(4);
+
+            Assert.True("IV" == roman, $"Roman numeral {roman} for 4 is incorrect");
         }
 
         [Fact]
         public void ShouldBeVI()
         {
             var challenge = new Challenge();
-            Assert.True("VI" == challenge.Numerals(6), "Roman numeral for 6 is incorrect");
+            var roman = challenge.Numerals(6);
+
+            Assert.True("VI" == roman, $"Roman numeral {roman} for 6 is incorrect");
         }
 
         [Fact]
         public void ShouldBeLXXXIX()
         {
             var challenge = new Challenge();
-            Assert.True("LXXXIX" == challenge.Numerals(89), "Roman numeral for 89 is incorrect");
+            var roman = challenge.Numerals(89);
+
+            Assert.True("LXXXIX" == roman, $"Roman numeral {roman} for 89 is incorrect");
         }
 
         [Fact]
         public void ShouldBeXCI()
         {
             var challenge = new Challenge();
-            Assert.True("XCI" == challenge.Numerals(91), "Roman numeral for 91 is incorrect");
+            var roman = challenge.Numerals(91);
+
+            Assert.True("XCI" == roman, $"Roman numeral {roman} for 91 is incorrect");
         }
 
         [Fact]
         public void ShouldBeMDCCCLXXXIX()
         {
             var challenge = new Challenge();
-            Assert.True("MDCCCLXXXIX" == challenge.Numerals(1889), "Roman numeral for 1889 is incorrect");
+            var roman = challenge.Numerals(1889);
+
+            Assert.True("MDCCCLXXXIX" == roman, $"Roman numeral {roman} for 1889 is incorrect");
         }
     }
 }
